3dfb35860a56eaaf821649a60c876ddc9e346b0e
yaoxiaobing <xiaobing.yao@duobei.com>
2019-12-17 10:26:47 +0800

    Initial Commit

---
+++ MenuBar-Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBar-Demo
+//
+//  Created by yxibng on 2019/12/17.
+//  Copyright © 2019 yxibng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
